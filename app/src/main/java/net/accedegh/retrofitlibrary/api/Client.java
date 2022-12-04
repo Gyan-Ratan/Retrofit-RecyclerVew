eeae3487e9b77bcad98737e1ad008e1ce6dc4d09
GYAN-RATAN <91837982+Gyan-Ratan@users.noreply.github.com>
2022-12-04 18:08:22 +0530

    Update Client.java

--- app/src/main/java/net/accedegh/retrofitlibrary/api/Client.java
+++ app/src/main/java/net/accedegh/retrofitlibrary/api/Client.java
@@ -7,9 +7,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by frank on 12/16/16.
  */
 
-public class Client {
+public class client {
 
-    public static final String BASE_URL = "http://orionadmin.azurewebsites.net";
+    // Trailing slash is needed
+    public static final String BASE_URL = "http://orionadmin.azurewebsites.net/";
     public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
